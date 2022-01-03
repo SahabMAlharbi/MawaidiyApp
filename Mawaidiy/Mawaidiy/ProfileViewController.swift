dd45944ef899f87993d1b14ade08cd7caf867dfb
SahabMAlharbi <Sahab.Mubarak.ruh.s@tuwaiq.edu.sa>
2022-01-03 13:34:54 +0300

    SignUp Done

--- Mawaidiy/Mawaidiy/ProfileViewController.swift
+++ Mawaidiy/Mawaidiy/ProfileViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import Firebase
 
 class ProfileViewController: UIViewController {
 
     @IBOutlet weak var pName: UILabel!
     @IBOutlet weak var idnumber: UILabel!
+    var users = UsersViewController()
+    let firestore = Firestore.firestore()
+    let userID = Auth.auth().currentUser?.uid
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        DispatchQueue.global(qos: .userInitiated).async {
+            DispatchQueue.main.async {
+                self.firestore.collection("Users")
+                    .whereField("userID", isEqualTo: self.userID as Any)
+                    .getDocuments { querySnapshot, error in
+                        if let error = error{
+                            print("wrong read!!")
+                            print(error.localizedDescription)
+                        }else {
+                            guard querySnapshot != nil else{return}
+                            for document in querySnapshot!.documents{
+                                let data = document.data()
+                                print(data["name"] as? String ?? "")
+                                print(data["ID Number"] as? String ?? "")
+                                self.pName.text = data["name"] as? String ?? ""
+                                self.idnumber.text = data["ID Number"] as? String ?? ""
+                                
+                            }
+                        }
+                    }
+            }
+        }
         // Do any additional setup after loading the view.
     }
     
---
+++ Mawaidiy/Mawaidiy/UsersViewController.swift
@@ -0,0 +1,55 @@
+//
+//  UsersViewController.swift
+//  Mawaidiy
+//
+//  Created by Sahab Alharbi on 30/05/1443 AH.
+//
+
+import UIKit
+import Firebase
+
+class UsersViewController: UIViewController {
+
+    let firestore = Firestore.firestore()
+    let userID = Auth.auth().currentUser?.uid
+    var users = [Users]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    func loadData(){
+        firestore.collection("Users").addSnapshotListener { (QuerySnapshot, error) in
+            guard let document = QuerySnapshot?.documents else{
+                return
+            }
+            self.users = document.map({ (QueryDocumentSnapshot) -> Users in
+                let data = QueryDocumentSnapshot.data()
+                let id = data["userID"] as? String ?? ""
+                let name = data["name"] as? String ?? "NO VALUE"
+                let email = data["email"] as? String ?? "NO VALUE"
+                let age = data["age"] as? String ?? "NO VALUE"
+                let idnumber = data["ID Number"] as? String ?? "NO VALUE"
+                return Users(id: id,name: name, email: email,age: age,idnumber: idnumber)
+            })
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+struct Users: Identifiable {
+    var id : String = UUID().uuidString
+    var name : String
+    var email : String
+    var age : String
+    var idnumber : String
+}
