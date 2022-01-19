d6b4a2edd8bbea500f19b6d7ca4fde59f5d5dceb
SahabMAlharbi <Sahab.Mubarak.ruh.s@tuwaiq.edu.sa>
2022-01-19 12:42:16 +0300

    done

--- Mawaidiy/Mawaidiy/ProfileViewController.swift
+++ Mawaidiy/Mawaidiy/ProfileViewController.swift
@@ -16,12 +16,10 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var pName: UILabel!
     @IBOutlet weak var idnumber: UILabel!
     @IBOutlet weak var mawidL: UILabel!
-//    var users = UsersViewController()
     let firestore = Firestore.firestore()
     let user = Auth.auth().currentUser?.uid
     override func viewDidLoad() {
         super.viewDidLoad()
-//        getinfo()
             DispatchQueue.main.async {
                 self.firestore.collection("Users")
                     .whereField("userID", isEqualTo: self.user )
@@ -52,64 +50,17 @@ class ProfileViewController: UIViewController {
                     }
             }
         
-        
-//        guard let currentUser = FirebaseAuth.Auth.auth().currentUser else {return}
-//        firestore.collection("Users").whereField("userid", isEqualTo: user!)
-//            .addSnapshotListener { (querySnapshot, error) in
-//                if let e = error {
-//                    print("There was an issue retrieving data from Firestore. \(e)")
-//                } else {
-//                    if let snapshotDocuments = querySnapshot?.documents {
-//                        print(snapshotDocuments)
-//                        for doc in snapshotDocuments {
-//                            let data = doc.data()
-//                            self.pName.text = data["name"] as? String
-//                            self.idnumber.text = data["ID Number"] as? String
-//                            self.phoneN.text = data["Phone Number"] as? String
-//                            self.ageL.text = data["age"] as? String
-//                            self.emailL.text = data["email"] as? String
-//
-//
-////                                DispatchQueue.main.async {
-////                                    self.pName.text = userName
-////                                    self.idnumber.text = idnumber
-////                                    self.phoneN.text = phoneN
-////                                    self.ageL.text = ageL
-////                                    self.emailL.text = emailL
-////                                }
-//
-//
-//
-//                        }
-//                    }
-//                }
-//
-//            }
-
-//        DispatchQueue.global(qos:  .userInitiated).async {
-//            DispatchQueue.main.async {
-//                self.firestore.collection("Users")
-//                    .whereField("userid", isEqualTo: self.user)
-//                    .getDocuments { querySnapshot, error in
-//                        if let error = error{
-//                            print("wrong read!!")
-//                            print(error.localizedDescription)
-//                        }else {
-//                            guard querySnapshot != nil else{return}
-//                            for document in querySnapshot!.documents{
-//                                let data = document.data()
-//                                print(data)
-//                                self.pName.text = data["name"] as? String ?? ""
-//                                self.idnumber.text = data["ID Number"] as? String ?? ""
-//                                self.phoneN.text = data["Phone Number"] as? String ?? ""
-//                                self.ageL.text = data["age"] as? String ?? ""
-//                                self.emailL.text = data["email"] as? String ?? ""
-//
-//                            }
-//                        }
-//                    }
-//            }
-//        }
+        mawidL.layer.cornerRadius = 10
+        ageL.layer.cornerRadius = 10
+        ageL.layer.masksToBounds = true
+        pName.layer.cornerRadius = 10
+        pName.layer.masksToBounds = true
+        phoneN.layer.cornerRadius = 10
+        phoneN.layer.masksToBounds = true
+        idnumber.layer.cornerRadius = 10
+        idnumber.layer.masksToBounds = true
+        emailL.layer.cornerRadius = 10
+        emailL.layer.masksToBounds = true
         // Do any additional setup after loading the view.
         let gesture = UITapGestureRecognizer(target: self, action: #selector(userTappedOnLink))
         // if labelView is not set userInteractionEnabled, you must do so
@@ -146,49 +97,6 @@ class ProfileViewController: UIViewController {
         vc.modalPresentationStyle = .fullScreen
         self.present(vc, animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-//    func getinfo(){
-//        firestore.collection("Users").whereField("userid", isEqualTo: user!)
-//            .addSnapshotListener { (querySnapshot, error) in
-//                if let e = error {
-//                    print("There was an issue retrieving data from Firestore. \(e)")
-//                } else {
-//                    if let snapshotDocuments = querySnapshot?.documents {
-//                        print(snapshotDocuments)
-//                        for doc in snapshotDocuments {
-//                            let data = doc.data()
-//                            self.pName.text = data["name"] as? String
-//                            self.idnumber.text = data["ID Number"] as? String
-//                            self.phoneN.text = data["Phone Number"] as? String
-//                            self.ageL.text = data["age"] as? String
-//                            self.emailL.text = data["email"] as? String
-//
-//
-////                                DispatchQueue.main.async {
-////                                    self.pName.text = userName
-////                                    self.idnumber.text = idnumber
-////                                    self.phoneN.text = phoneN
-////                                    self.ageL.text = ageL
-////                                    self.emailL.text = emailL
-////                                }
-//
-//
-//
-//                        }
-//                    }
-//                }
-//
-//            }
-//    }
 }
 
 
